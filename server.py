f92e373e6665966bcf593a0664a9feb16153de3b
Covek01 <84289288+Covek01@users.noreply.github.com>
2024-06-01 17:54:25 +0200

    update with BPL

--- server.py
+++ server.py
@@ -1,85 +1,139 @@
-import sys
+
 import socket
 import time
 import threading
-import requests
 from datetime import datetime
-from airplane import Airplane
-import json
 import functools as f
 
-def generate_airplanes(num_airplanes):
-    return [Airplane(f"AB{123+i}") for i in range(num_airplanes)]
 
-def handle_client(client_socket, airplanes):
-    url = "https://opensky-network.org/api/states/all"
-    params = {
-        "lamax": 44.75,
-        "lomin": 20.47637,
-        "lamin": 35.51353,
-        "lomax": 21.83642
-    }
+
+def handle_client(client_socket):
     try:
+        arsenal = open("./database/arsenal.csv", "r")
+        aston_villa = open("./database/aston_villa.csv", "r")
+        bournemouth = open("./database/bournemouth.csv", "r")
+        brentford = open("./database/brentford.csv", "r")
+        brighton = open("./database/brighton.csv", "r")
+        burnley = open("./database/burnley.csv", "r")
+        chelsea = open("./database/chelsea.csv", "r")
+        crystal_palace = open("./database/crystal_palace.csv", "r")
+        everton = open("./database/everton.csv", "r")
+        fulham = open("./database/fulham.csv", "r")
+        liverpool = open("./database/liverpool.csv", "r")
+        luton = open("./database/luton.csv", "r")
+        manchester_city = open("./database/manchester_city.csv", "r")
+        manchester_united = open("./database/manchester_united.csv", "r")
+        newcastle_united = open("./database/newcastle_united.csv", "r")
+        nottingham_forest = open("./database/nottingham_forest.csv", "r")
+        sheffield_united = open("./database/sheffield_united.csv", "r")
+        tottenham_hotspur = open("./database/tottenham_hotspur.csv", "r")
+        west_ham_united = open("./database/west_ham_united.csv", "r")
+        wolverhampton_wanderers = open("./database/wolverhampton_wanderers.csv", "r")
+
+
+        dataArsenal = arsenal.readline()
+        dataAstonVilla = aston_villa.readline()
+        dataBournemouth = bournemouth.readline()
+        dataBrentford = brentford.readline()
+        dataBrighton = brighton.readline()
+        dataBurnley = burnley.readline()
+        dataChelsea = chelsea.readline()
+        dataCrystalPalace = crystal_palace.readline()
+        dataEverton = everton.readline()
+        dataFulham = fulham.readline()
+        dataLiverpool = liverpool.readline()
+        dataLuton = luton.readline()
+        dataManchesterCity = manchester_city.readline()
+        dataManchesterUnited = manchester_united.readline()
+        dataNewcastleUnited = newcastle_united.readline()
+        dataNottinghamForest = nottingham_forest.readline()
+        dataSheffieldUnited = sheffield_united.readline()
+        dataTottenhamHotspur = tottenham_hotspur.readline()
+        dataWestHamUnited = west_ham_united.readline()
+        dataWolverhamptonWanderers = wolverhampton_wanderers.readline()
+
+
         while True:
-            values_list = list()
-            for airplane in airplanes:
-                airplane.update()
-            utc_time = datetime.now().isoformat()
-            # response = requests.get(url, params=params)
-            # if (response.status_code == 200):
-            #     data = response.json()
-            #     date = data.date
-            #     utc_time = datetime.fromtimestamp(date)
+                    # Read the first line from each file
+            dataArsenal = arsenal.readline()
+            dataAstonVilla = aston_villa.readline()
+            dataBournemouth = bournemouth.readline()
+            dataBrentford = brentford.readline()
+            dataBrighton = brighton.readline()
+            dataBurnley = burnley.readline()
+            dataChelsea = chelsea.readline()
+            dataCrystalPalace = crystal_palace.readline()
+            dataEverton = everton.readline()
+            dataFulham = fulham.readline()
+            dataLiverpool = liverpool.readline()
+            dataLuton = luton.readline()
+            dataManchesterCity = manchester_city.readline()
+            dataManchesterUnited = manchester_united.readline()
+            dataNewcastleUnited = newcastle_united.readline()
+            dataNottinghamForest = nottingham_forest.readline()
+            dataSheffieldUnited = sheffield_united.readline()
+            dataTottenhamHotspur = tottenham_hotspur.readline()
+            dataWestHamUnited = west_ham_united.readline()
+            dataWolverhamptonWanderers = wolverhampton_wanderers.readline()
 
+            client_socket.send(dataArsenal.encode())
+            client_socket.send(dataAstonVilla.encode())
+            client_socket.send(dataBournemouth.encode())
+            client_socket.send(dataBrentford.encode())
+            client_socket.send(dataBrighton.encode())
+            client_socket.send(dataBurnley.encode())
+            client_socket.send(dataChelsea.encode())
+            client_socket.send(dataCrystalPalace.encode())
+            client_socket.send(dataEverton.encode())
+            client_socket.send(dataFulham.encode())
+            client_socket.send(dataLiverpool.encode())
+            client_socket.send(dataLuton.encode())
+            client_socket.send(dataManchesterCity.encode())
+            client_socket.send(dataManchesterUnited.encode())
+            client_socket.send(dataNewcastleUnited.encode())
+            client_socket.send(dataNottinghamForest.encode())
+            client_socket.send(dataSheffieldUnited.encode())
+            client_socket.send(dataTottenhamHotspur.encode())
+            client_socket.send(dataWestHamUnited.encode())
+            client_socket.send(dataWolverhamptonWanderers.encode())
 
-                #0 - Unique 24b address
-                #1 - callsign
-                #2 - country
-                #5 - longitude
-                #6 - latitude
-                #7 - barometric altitude
-                #8 - on ground
-                #9 - velocity
-                #13 - geo altitude
-                # results = map(lambda state:
-                #         [utc_time, state[0], state[1], state[2], state[5], state[6], state[7], state[8], state[9], state[13]], data.states)
-            results = list(map(lambda state:
-                    [utc_time, *[value for value in state.__dict__.values()][:-1]], airplanes))
-            for result in results:
-                resultsString = f.reduce(lambda i, j: str(i) + ", " + str(j), result)
-                # print(resultsString)
-                sent = client_socket.send((resultsString + '\n').encode('utf-8'))
-                if (sent == False):
-                    print("BAD SENDING DATA")
-
-            time.sleep(5)
+            if (dataArsenal == '' or dataAstonVilla == '' or dataBournemouth == '' or dataBrentford == '' or 
+            dataBrighton == '' or dataBurnley == '' or dataChelsea == '' or dataCrystalPalace == '' or 
+            dataEverton == '' or dataFulham == '' or dataLiverpool == '' or dataLuton == '' or 
+            dataManchesterCity == '' or dataManchesterUnited == '' or dataNewcastleUnited == '' or 
+            dataNottinghamForest == '' or dataSheffieldUnited == '' or dataTottenhamHotspur == '' or 
+            dataWestHamUnited == '' or dataWolverhamptonWanderers == ''):
+                break
             
-    
-            # client_socket.send(resultsString.encode())
-            # if (response.status_code == 429):
-            #     break
-    finally:
-        client_socket.close()
-    # with open(data_file_path, mode="r", encoding='utf-8-sig') as data_file:
-    #     lines = data_file.readlines()
-    #     try:
-    #         i = 0
-    #         while i< len(lines):
-    #             print("sending")
-    #             client_socket.send(lines[i].encode())
-    #             client_socket.send(lines[i+1].encode())
-    #             client_socket.send(lines[i+2].encode())
-    #             client_socket.send(lines[i+3].encode())
-    #             i = i+4
-    #             time.sleep(5)
-    #     except:
-    #         print("error")
-    #     finally:
-    #         print("closing")
-    #         client_socket.close()
+            time.sleep(5)
 
-def start_server(host: str, port: int, num_of_planes: int):
-    airplanes = generate_airplanes(num_of_planes)
+    finally:
+        arsenal.close()
+        aston_villa.close()
+        bournemouth.close()
+        brentford.close()
+        brighton.close()
+        burnley.close()
+        chelsea.close()
+        crystal_palace.close()
+        everton.close()
+        fulham.close()
+        liverpool.close()
+        luton.close()
+        manchester_city.close()
+        manchester_united.close()
+        newcastle_united.close()
+        nottingham_forest.close()
+        sheffield_united.close()
+        tottenham_hotspur.close()
+        west_ham_united.close()
+        wolverhampton_wanderers.close()
+
+        client_socket.close()
+
+
+
+def start_server(host: str, port: int):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen()
@@ -88,7 +142,7 @@ def start_server(host: str, port: int, num_of_planes: int):
     while True:
         client_socket, address = server_socket.accept()
         print(f"Client connected from {address[0]}:{address[1]}")
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, airplanes))
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
         client_thread.start()
 
 
@@ -100,4 +154,4 @@ if __name__ == "__main__":
     
     host = 'localhost' 
     port = 9999  
-    start_server(host, port, 5)
+    start_server(host, port)
